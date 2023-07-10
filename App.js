d91e217581fb611fbb721e018b0c39cec0020fa9
Manukrishna2000 <manukrishnaap@gmail.com>
2023-07-10 15:49:40 +0530

    gh

--- App.js
+++ App.js
@@ -3,15 +3,21 @@ const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-app.use(cors())
 
-
+app.use((req, res, next) => {
+  // Replace 'https://64abd99e9090834482ade6f7--coruscating-beignet-01c9f0.netlify.app/' 
+  // with the actual origin from which you are making the request 
+  res.setHeader('Access-Control-Allow-Origin', 'https://64abd99e9090834482ade6f7--coruscating-beignet-01c9f0.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-    }
-  });
+  cors: {
+    origin: '*',
+  }
+});
 
 // Define a socket.io connection event
 io.on('connection', (socket) => {
@@ -30,9 +36,6 @@ io.on('connection', (socket) => {
     console.log('A user disconnected');
   });
 });
-
-
-
 
 server.listen(3001, () => {
   console.log('Server is running on http://localhost:3001');
